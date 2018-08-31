8df041c06e1e4dc2905076c692aef9bcecc9d202
tahirkhan109 <tahir_lodhi109@yahoo.com>
2018-08-31 22:58:42 +0500

    pushed design  of bootstrap

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,91 @@
 module ApplicationHelper
+  CELEBVIDY_FEE = 0.75
+
+  def flash_class(level)
+    case level
+      when "notice" then
+        "alert alert-info"
+      when "success" then
+        "alert alert-success"
+      when "error" then
+        "alert alert-error"
+      when "alert" then
+        "alert alert-error"
+      when "danger" then
+        "alert alert-danger"
+    end
+  end
+
+  # JOBS_STATUS = ["completed", "pending", "rejected", "incomplete", "notdelivered"]
+  # BUGS_TYPES = ["Question", "Order Issue", "Site/App Bug", "PR and Partnership"]
+  # NUMBERS = [10,20,30,40,50,60,70,80,90,100]
+  # Completed = All tasks completed by users and video is delivered as from to user.
+  # Pending = All task are completed by user , payment is made , now celebrity need to create video.
+  # Rejected = All tasks completed by users , payment was made but Celebrity/Admin rejected this job request.
+  # Incomplete = Only Jobs was created but payment is still due. User needs to complete this.
+  # NotDelivered = Jobs was created payment was made but video was not delivered before on time.
+  # def title(page_title)
+  #   content_for(:title) { page_title }
+  # end
+
+  USSTATES =[
+      ['Alabama', 'AL'],
+      ['Alaska', 'AK'],
+      ['Arizona', 'AZ'],
+      ['Arkansas', 'AR'],
+      ['California', 'CA'],
+      ['Colorado', 'CO'],
+      ['Connecticut', 'CT'],
+      ['Delaware', 'DE'],
+      ['District of Columbia', 'DC'],
+      ['Florida', 'FL'],
+      ['Georgia', 'GA'],
+      ['Hawaii', 'HI'],
+      ['Idaho', 'ID'],
+      ['Illinois', 'IL'],
+      ['Indiana', 'IN'],
+      ['Iowa', 'IA'],
+      ['Kansas', 'KS'],
+      ['Kentucky', 'KY'],
+      ['Louisiana', 'LA'],
+      ['Maine', 'ME'],
+      ['Maryland', 'MD'],
+      ['Massachusetts', 'MA'],
+      ['Michigan', 'MI'],
+      ['Minnesota', 'MN'],
+      ['Mississippi', 'MS'],
+      ['Missouri', 'MO'],
+      ['Montana', 'MT'],
+      ['Nebraska', 'NE'],
+      ['Nevada', 'NV'],
+      ['New Hampshire', 'NH'],
+      ['New Jersey', 'NJ'],
+      ['New Mexico', 'NM'],
+      ['New York', 'NY'],
+      ['North Carolina', 'NC'],
+      ['North Dakota', 'ND'],
+      ['Ohio', 'OH'],
+      ['Oklahoma', 'OK'],
+      ['Oregon', 'OR'],
+      ['Pennsylvania', 'PA'],
+      ['Puerto Rico', 'PR'],
+      ['Rhode Island', 'RI'],
+      ['South Carolina', 'SC'],
+      ['South Dakota', 'SD'],
+      ['Tennessee', 'TN'],
+      ['Texas', 'TX'],
+      ['Utah', 'UT'],
+      ['Vermont', 'VT'],
+      ['Virginia', 'VA'],
+      ['Washington', 'WA'],
+      ['West Virginia', 'WV'],
+      ['Wisconsin', 'WI'],
+      ['Wyoming', 'WY']
+  ]
+
+
+
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,17 @@ Rails.application.routes.draw do
   }
 
   resources :home
+  devise_scope :user do
+    #get "admin", :to => "devise/sessions#new"
+    #get "sign-in", :to => "users/sessions#new"
+    # get 'sign_in', :to => 'users/sessions#new', :as => :new_session
+    get "/users/sign_out", :to => "users/sessions#destroy"
+    get "/users/admin_sign_in", :to => "users/sessions#admin_sign_in"
+    #post "sign-out", :to => "users/sessions#destroy"
+    #get "sign-up", :to => "users/registrations#new"
+    # patch "/users/update_user", :to =s> "users/registrations#update_user"
+    #get "login", :to => "devise/sessions#login"
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
